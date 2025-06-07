1154c6f8779e2201101f64e715c577b98b518098
Mark Moffat <hi@markmoffat.com>
2025-06-07 14:08:34 +0930

    Fixes

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ set -e
 echo "Starting Strava Statistics add-on..."
 
 # Ensure necessary directories are writable
+mkdir -p /var/www/storage/database
+mkdir -p /var/www/storage/files
+
 chmod -R 777 /var/www/storage/database
 chmod -R 777 /var/www/storage/files
 
